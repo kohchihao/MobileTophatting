2044eca4ca26790828c424b5565bd1b8af9478f8
Marcus Koh <kohchihao@gmail.com>
2023-01-08 20:09:11 +0800

    feat: add ios bundle loader

---
+++ ios/Tophatting/RNDevManager.h
@@ -0,0 +1,17 @@
+//
+//  RNDevManager.h
+//  Tophatting
+//
+//  Created by Marcus Koh on 8/1/23.
+//
+
+
+#import <React/RCTBridgeModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNDevManager : NSObject <RCTBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
